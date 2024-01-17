942e44b2b6b78d3c50b7d13b52ab922634312635
James Gibson <jgibson@neworbit.co.uk>
2024-01-17 15:57:44 +0000

    Converted to default validator and added initial data

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import Form from '@rjsf/mui';
 import React, { useEffect } from 'react';
 import { schema } from './schema';
-import { customizeValidator } from '@rjsf/validator-ajv8';
+import validator from '@rjsf/validator-ajv8';
+
+const initialData = {
+  "DataSource":"chalgrove-cartularies",
+  "Surname":"Albus",
+  "FirstName":"William",
+  "Location":"Abingdon't",
+  "Date":1220
+};
 
 export const App = () => {
   useEffect(() => console.info({ schema }), []);
   return (
-    <>
-      <Form schema={schema} validator={customizeValidator({
-        ajvOptionsOverrides: {
-          allErrors: true,
-          loopEnum: 1,
-          inlineRefs: false
-        }
-      })}></Form>
-    </>
-  )
+    <Form schema={schema} validator={validator} formData={initialData}></Form>
+  );
 }
--- src/schema.ts
+++ src/schema.ts
@@ -1,52 +1,63 @@
 import { RJSFSchema } from "@rjsf/utils";
 
 export const schema = JSON.parse(`{
-    "$id": "/surnames.json(bundled)",
-    "$defs": {
-        "surnames": {
-            "$schema": "https://json-schema.org/draft/2020-12/schema",
-            "$id": "/surnames.json",
-            "title": "Surname",
-            "description": "A row of surname data.",
-            "type": "object",
-            "properties": {
-                "Surname": {
-                    "description": "Surname",
-                    "columnName": "Surname",
-                    "type": "string",
-                    "includeInSearchList": true
-                },
-                "FirstName": {
-                    "description": "First name",
-                    "columnName": "First name",
-                    "type": "string",
-                    "includeInSearchList": true
-                },
-                "Location": {
-                    "description": "Location",
-                    "columnName": "Location",
-                    "$ref": "#/$defs/locations",
-                    "isFacet": true,
-                    "includeInSearchList": true
-                },
-                "Date": {
-                    "description": "Date",
-                    "columnName": "Date",
-                    "type": "integer",
-                    "isFacet": true,
-                    "facetStepSize": "20",
-                    "includeInSearchList": true
-                }
-            }
-        },
-        "locations": {
-            "$schema": "https://json-schema.org/draft/2020-12/schema",
-            "$id": "/locations.json",
-            "title": "Location",
-            "description": "A type representing supported locations.",
-            "type": "string",
-            "enum": ["Abingdon", "Cumnor", "Oxford", "Oxfordshire"]
-        }
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "$id": "surnames-v1.1.0.json",
+    "title": "Surname",
+    "description": "A row of surname data.",
+    "type": "object",
+    "required": [
+      "Surname"
+    ],
+    "properties": {
+      "DataSource": {
+        "description": "Data Source",
+        "columnName": "Data source",
+        "type": "string",
+        "includeInSearchList": false,
+        "isDataSource": true
+      },
+      "Surname": {
+        "description": "Surname",
+        "columnName": "Surname",
+        "type": "string",
+        "includeInSearchList": true
+      },
+      "FirstName": {
+        "description": "First name",
+        "columnName": "First name",
+        "type": "string",
+        "includeInSearchList": true
+      },
+      "Location": {
+        "description": "Location",
+        "columnName": "Location",
+        "$ref": "#/$defs/locations.json",
+        "isFacet": true,
+        "includeInSearchList": true
+      },
+      "Date": {
+        "description": "Date",
+        "columnName": "Date",
+        "type": "integer",
+        "isFacet": true,
+        "facetStepSize": "20",
+        "includeInSearchList": true
+      }
     },
-    "$ref": "#/$defs/surnames"
-}`) as RJSFSchema;
+    "$defs": {
+      "locations.json": {
+        "$schema": "https://json-schema.org/draft/2020-12/schema",
+        "$id": "locations.json",
+        "title": "Location",
+        "description": "A type representing supported locations.",
+        "type": "string",
+        "enum": [
+          "Abingdon",
+          "Cumnor",
+          "Oxford",
+          "Oxfordshire"
+        ]
+      }
+    }
+  }`) as RJSFSchema;
